e79925c16ced1705e31426ddfa30792c912fd2ac
Nicholas Chen <grahmnic@gmail.com>
2019-08-09 14:37:27 -0400

    Reverting debug mode

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      verified: true,
+      verified: false,
       loading: false
     }
     this.loadingOptions = {
